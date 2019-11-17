b3e3646da69968e3c3d4da95bcef6823efff1192
atharvapatil <atharvaabhaypatil@gmail.com>
2019-11-17 15:25:29 -0500

    teachable rearchitecture setup complete

---
+++ js/teachable-machine.js
@@ -0,0 +1,5 @@
+console.log("javaScript file initiated");
+
+window.addEventListener('load', () => {
+  document.getElementById('text').textContent = 'What does the fox say?';
+});
--- sketch.js
+++ sketch.js
@@ -1,8 +1,8 @@
-// const mySoundModelURL = 'https://storage.googleapis.com/teachable-machine-pubilshed-models/df7542cb-b60e-4187-bfcd-61efdecb9467/model.json';
 const mySoundModelURL = 'https://storage.googleapis.com/teachable-machine-pubilshed-models/f54c5740-c672-4e7c-aac8-a68007189ead/model.json';
 let mySoundModel;
 let resultDiv;
 let voiceResult;
+let voiceResultConfidence;
 
 function preload() {
   mySoundModel = ml5.soundClassifier(mySoundModelURL);
@@ -17,17 +17,20 @@ function gotResults(err, results) {
   if (err) console.log(err);
   if (results) {
     voiceResult = results[0].label;
+    voiceResultConfidence = results[0].confidence;
     console.log(results[1].label + ' ' + results[1].confidence*100);
 
     document.getElementById('results').innerHTML = voiceResult;
 
-    //Math.abs converts a negative number to a positive one
-    if (voiceResult == 'open') {
-      removeBlur();
-    }
+    // document.getElementById('video').src = "https://www.youtube.com/embed/4RpZ1mwrceg";
 
-    if (voiceResult == 'close' || voiceResult == '_background_noise_') {
+    //Math.abs converts a negative number to a positive one
+    if (voiceResult == 'open' && voiceResultConfidence > 0.7) {
+      removeBlur();
+    }  if (voiceResult == 'close' && voiceResultConfidence > 0.7) {
       blurScreen();
+    }  if (voiceResult == '_background_noise_') {
+      removeBlur();
     }
 
   }
