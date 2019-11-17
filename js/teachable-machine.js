2225ddda677826539466e9fe6265511a5797d214
atharvapatil <atharvaabhaypatil@gmail.com>
2019-11-17 17:36:47 -0500

    v1 styles

--- js/teachable-machine.js
+++ js/teachable-machine.js
@@ -1,49 +1,42 @@
 console.log("javaScript file initiated");
+// Normal Youtube link & Embedded youtube url samples for reference
+// const EMBED_URL = 'https://www.youtube.com/embed/dQw4w9WgXcQ';
+// const VIDEO_URL = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
 
 window.addEventListener('load', () => {
   document.getElementById('text').textContent = 'What does the fox say?';
   // document.body.style.filter = 'blur(1px)';
 });
 
+function handleVideoUpdate(){
+  //declaring common constant params
+  const YOUTUBE_URL = 'https://www.youtube.com/';
+  const EMBED_URL = 'embed/';
 
-function stringTest(){
-  // let inputString = 'https://www.youtube.com/watch?v=Ez89l1NTFXU';
+  //getting input url from user input
+  let userVideoURL = document.getElementById('youtube-url').value;
 
-  let inputString = document.getElementById('youtube-url').value;
+  //splitting the input string at the '=' sign to make it a embed compatible URL
+  let splitVideoURL = userVideoURL.split('=');
 
-  let splitString = inputString.split('=');
+  //The unique video key is the 2nd element of the splitVideoURL array
+  let videoKey = splitVideoURL[1];
 
-  console.log(splitString);
+  //The new url is a contactnation of YOUTUBE_URL + EMBED_URL + videoKey
+  let newURL = YOUTUBE_URL + EMBED_URL + videoKey;
 
-  const BASE_COMMON_URL = 'https://www.youtube.com/';
-  const TO_REPLACE = 'embed/';
-  let videoURL = splitString[1];
+  // For debugging
+  // console.log(newURL);
 
-  let newURL = BASE_COMMON_URL + TO_REPLACE + videoURL;
-
-  console.log(newURL);
-
+  //updating the embedded iframes source
   document.getElementById('embed-video').src = newURL;
-}
-
-
-function testFunction(){
-  const inputValue = document.getElementById('youtube-url').value;
-  document.getElementById('text').textContent = inputValue;
-  console.log(inputValue);
-
-  return inputValue;
-}
-
-
-function videoURLUpdate(){
 
 }
-
-// const EMBED_URL = 'https://www.youtube.com/embed/_t0ZBAk72K8';
-// const VIEO_URL = 'https://www.youtube.com/watch?v=_t0ZBAk72K8';
 //
+// function testFunction(){
+//   const inputValue = document.getElementById('youtube-url').value;
+//   document.getElementById('text').textContent = inputValue;
+//   console.log(inputValue);
 //
-// const BASE_COMMON_URL = 'https://www.youtube.com/';
-// const TO_REMOVE = 'watch?v=';
-// const TO-REPLACE = 'embed/';
+//   return inputValue;
+// }
